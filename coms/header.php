82d51c628e790c62bf2d4a8639236ebb7ce4d880
Alex_Green <deadpoolweid@yandex.ru>
2016-07-11 14:37:40 +0300

    v 0.1.0.4 (unstable)
    - Обновление стилей для шапки сайта
    - Начало работы над обновлением стилей всего сайта

--- coms/header.php
+++ coms/header.php
@@ -1,15 +1,16 @@
   <!-- header begins -->
+  <link href="/coms/header.css" rel="stylesheet" type="text/css">
   <div class="header">
       <nav id="buttons">
-          <a href="../index.php" class="but" >Домашняя</a>
+          <a class="but" href="../index.php">Домашняя</a>
           <a class="but" href="../media.php">Медиа</a>
           <a class="but" href="../gallery.php">Галерея</a>
           <a class="but" href="../about_us.php">О себе</a>
           <a class="but" href="../contact_us.php">Контакты</a>
       </nav>
       <div id="logo">
-          <a>Колесников Александр</a>
-          <h2><a><small>Долгий путь начинается с малого</small></a></h2>
+          <a>Alex Green</a>
+          <h2><small>Долгий путь начинается с малого</small></h2>
       </div>
   </div>
   <!-- header ends -->
