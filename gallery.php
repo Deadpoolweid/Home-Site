7f267dcc71fcb1487d6f2f917be8009ef0c81cfd
Alex_Green <deadpoolweid@yandex.ru>
2016-08-10 19:16:23 +0300

    v 0.1.4.1
    - Небольшие исправление в галерее и медиа

--- gallery.php
+++ gallery.php
@@ -7,107 +7,109 @@
 </head>
 
 <body>
-<div id="main">
+<section>
     <? require_once "coms/header.php" ?>
     <!-- content begins -->
-    <div id="content">
-        <div class="row">
-            <div class="cell">
-                <a href="assets/images/gallery_big1.jpg" title="Японская живопись">
-                    <img src="assets/images/img31.jpg" alt=""/>
-                </a>
-                <h1>Японская живопись</h1>
-                <p>
-                    Для японской живописи характерно отведение ведущего места природе и изображение её в качестве
-                    носительницы божественного начала.
-                </p>
+    <section>
+        <div class="table">
+            <div class="row">
+                <div class="cell">
+                    <a href="assets/images/gallery_big1.jpg" title="Японская живопись">
+                        <img src="assets/images/img31.jpg" alt=""/>
+                    </a>
+                    <h2>Японская живопись</h2>
+                    <p>
+                        Для японской живописи характерно отведение ведущего места природе и изображение её в качестве
+                        носительницы божественного начала.
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big2.jpg" title="Архитектура">
+                        <img src="assets/images/img32.jpg" alt=""/>
+                    </a>
+                    <h2>Архитектура</h2>
+                    <p>
+                        Традиционные деревянные жилые дома простых японцев.
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big3.jpg" title="Кухня">
+                        <img src="assets/images/img33.jpg" alt=""/>
+                    </a>
+                    <h2>Кухня</h2>
+                    <p>
+                        Одно из наиболее популярных за пределами страны японских блюд — суши.
+                    </p>
+                </div>
             </div>
 
-            <div class="cell">
-                <a href="assets/images/gallery_big2.jpg" title="Архитектура">
-                    <img src="assets/images/img32.jpg" alt=""/>
-                </a>
-                <h1>Архитектура</h1>
-                <p>
-                    Традиционные деревянные жилые дома простых японцев.
-                </p>
+            <div class="row">
+                <div class="cell">
+                    <a href="assets/images/gallery_big4.jpg" title="Напитки">
+                        <img src="assets/images/img34.jpg" alt=""/>
+                    </a>
+                    <h2>Напитки</h2>
+                    <p>
+                        Традиционным и популярным напитком японцев является зелёный чай.
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big5.jpg" title="Пейзажи">
+                        <img src="assets/images/img35.jpg" alt=""/>
+                    </a>
+                    <h2>Пейзажи</h2>
+                    <p>
+                        Есть в Японии особенно любимые японцами цветы.
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big6.jpg" title="Оригами">
+                        <img src="assets/images/img36.jpg" alt=""/>
+                    </a>
+                    <h2>Оригами</h2>
+                    <p>
+                        Древнее искусство складывания фигурок из бумаги.
+                    </p>
+                </div>
             </div>
 
-            <div class="cell">
-                <a href="assets/images/gallery_big3.jpg" title="Кухня">
-                    <img src="assets/images/img33.jpg" alt=""/>
-                </a>
-                <h1>Кухня</h1>
-                <p>
-                    Одно из наиболее популярных за пределами страны японских блюд — суши.
-                </p>
+            <div class="row">
+                <div class="cell">
+                    <a href="assets/images/gallery_big7.jpg" title="Зима">
+                        <img src="assets/images/img37.jpg" alt=""/>
+                    </a>
+                    <h2>Зима</h2>
+                    <p>
+                        Японию стоит посетить даже зимой, несмотря на довольно таки нежаркие температуры воздуха.
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big8.jpg" title="Дракон">
+                        <img src="assets/images/img38.jpg" alt=""/>
+                    </a>
+                    <h2>Дракон</h2>
+                    <p>
+                        Один из известнейших японских драконов — Ямата-но ороти (山多の大蛇).
+                    </p>
+                </div>
+
+                <div class="cell">
+                    <a href="assets/images/gallery_big9.jpg" title="Аниме">
+                        <img src="assets/images/img39.jpg" alt=""/>
+                    </a>
+                    <h2>Аниме</h2>
+                    <p>
+                        Аниме (или японская анимация) имеет высокую популярность во всём мире.
+                    </p>
+                </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="cell">
-                <a href="assets/images/gallery_big4.jpg" title="Напитки">
-                    <img src="assets/images/img34.jpg" alt=""/>
-                </a>
-                <h1>Напитки</h1>
-                <p>
-                    Традиционным и популярным напитком японцев является зелёный чай.
-                </p>
-            </div>
-
-            <div class="cell">
-                <a href="assets/images/gallery_big5.jpg" title="Пейзажи">
-                    <img src="assets/images/img35.jpg" alt=""/>
-                </a>
-                <h1>Пейзажи</h1>
-                <p>
-                    Есть в Японии особенно любимые японцами цветы.
-                </p>
-            </div>
-
-            <div class="cell">
-                <a href="assets/images/gallery_big6.jpg" title="Оригами">
-                    <img src="assets/images/img36.jpg" alt=""/>
-                </a>
-                <h1>Оригами</h1>
-                <p>
-                    Древнее искусство складывания фигурок из бумаги.
-                </p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="cell">
-                <a href="assets/images/gallery_big7.jpg" title="Зима">
-                    <img src="assets/images/img37.jpg" alt=""/>
-                </a>
-                <h1>Зима</h1>
-                <p>
-                    Японию стоит посетить даже зимой, несмотря на довольно таки нежаркие температуры воздуха.
-                </p>
-            </div>
-
-            <div class="cell">
-                <a href="assets/images/gallery_big8.jpg" title="Дракон">
-                    <img src="assets/images/img38.jpg" alt=""/>
-                </a>
-                <h1>Дракон</h1>
-                <p>
-                    Один из известнейших японских драконов — Ямата-но ороти (山多の大蛇).
-                </p>
-            </div>
-
-            <div class="cell">
-                <a href="assets/images/gallery_big9.jpg" title="Аниме">
-                    <img src="assets/images/img39.jpg" alt=""/>
-                </a>
-                <h1>Аниме</h1>
-                <p>
-                    Аниме (или японская анимация) имеет высокую популярность во всём мире.
-                </p>
-            </div>
-        </div>
-    </div>
+    </section>
     <!-- content ends -->
-
+</section>
 <?php require_once "coms/footer.php" ?>
--- media.php
+++ media.php
@@ -7,35 +7,28 @@
     <? require_once "links.php" ?>
 </head>
 <body>
-<div id="main">
+<section id="main">
     <?php require_once "coms/header.php" ?>
 
     <!-- content begins -->
-    <div id="content">
-        <div id="left">
-            <object type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"
-                    data="Players/Player.swf" width="240" height="500">
-                <param name="movie" value="OriginalMusicPlayerPlaylist.swf"/>
-                <param name="FlashVars"
-                       value="firstColor=1507ea&secondColor=#8cc540&strokeColor=#c7b81d&playlistXmlPath=Players/playlist.xml"/>
-                <param name="wmode" value="opaque"/>
-            </object>
-        </div>
-        <div id="right">
-            <h1>DW Season 8 trailer</h1>
+    <section>
+        <aside>
+            <div>
+                <h1>Здесь будет плеер</h1>
+            </div>
+        </aside>
+        <article>
+            <h2>DW Season 8 trailer</h2>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/-lBeV5bQW70" frameborder="0"
-                    allowfullscreen></iframe>
-
+                    allowfullscreen></iframe> <br>
             <p>
                 By:
-                <b>
-                    <a href="http://www.youtube.com/channel/UCggccVQxXZFny58cconVANA">
-                        John Smith
-                    </a>
-                </b>
+                <a href="http://www.youtube.com/channel/UCggccVQxXZFny58cconVANA">
+                    John Smith
+                </a>
             </p>
-        </div>
-    </div>
+        </article>
+    </section>
     <!-- content ends -->
 
     <?php require_once "coms/footer.php" ?>
